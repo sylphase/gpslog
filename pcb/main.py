18b27ac5eb754086edb25765127c42aaba686622
Forrest Voight <forrest@forre.st>
2015-09-20 00:49:09 -0400

    work

--- pcb/main.py
+++ pcb/main.py
@@ -10,12 +10,11 @@ from autoee.components.resistor import resistor as _resistor
 from autoee_components.nvs_technologies_ag import NV08C_CSM
 from autoee_components.skytraq import S1315F8_RAW
 from autoee_components.skyworks import AAT3221
-from autoee_components._4ucon import _11029
 from autoee_components.molex import _0732511150
 from autoee_components.copal_electronics.CL_SB import CL_SB_22A_12
 from autoee_components.murata_electronics import BLM15G
-from autoee_components.laird_technologies import BMI_S_203
-from autoee_components.jst import PH
+from autoee_components.laird_technologies import BMI_S_202
+from autoee_components.jst import PH, SH
 from autoee_components.amphenol import _101_00660
 from autoee_components.bosch_sensortec import BNO055
 from autoee_components.measurement_specialties import MS5611_01BA03
@@ -27,6 +26,8 @@ from autoee_components.silicon_labs import Si501
 from autoee_components.hirose import U_FL
 from autoee_components.lite_on import LTST_C19HE1WT
 from autoee_components.toshiba import CUS520
+from autoee_components.taoglas import GP_1575_25_4_A_02
+from autoee_components.invensense import MPU_9250
 
 microstrip_width = 0.01198*INCH
 
@@ -64,7 +65,7 @@ def main():
     port_rxd = Net('port_rxd')
     port_rts = Net('port_rts')
     yield CUS520.CUS520_H3F('D1', A=vcc3_3, C=port_vcc)
-    connector = _11029.make_11029('gnd cts vcc txd rxd rts'.split(' '))
+    connector = SH.SM06B_SRSS_TB('gnd cts vcc txd rxd rts'.split(' '))
     yield connector('P1',
         gnd=gnd,
         cts=port_cts,
@@ -72,6 +73,7 @@ def main():
         txd=port_txd,
         rxd=port_rxd,
         rts=port_rts,
+        MECHANICAL=gnd,
     )
     
     # GPS RECEIVER & ANTENNA CONNECTORS
@@ -87,6 +89,7 @@ def main():
         SIG=ant1,
         GND=gnd,
     )
+    #yield GP_1575_25_4_A_02.GP_1575_25_4_A_02('A1', ANT=ant1)
     vin_a = Net('vin_a')
     yield BLM15G.BLM15GG471SN1D('FB1', A=vcc3_3, B=vin_a)
     yield capacitor(22e-12)('C1', A=vin_a, B=gnd)
@@ -117,7 +120,7 @@ def main():
     
     
     # RF SHIELD
-    yield BMI_S_203.BMI_S_203('SH', GND=gnd)
+    yield BMI_S_202.BMI_S_202('SH', GND=gnd)
     
     # STATUS LED
     # two color led for status
@@ -137,37 +140,44 @@ def main():
         CB=status_led_blue_cathode,
     )
     
-    # AHRS
-    ahrs_i2c = harnesses.I2CBus.new('ahrs_i2c_')
-    ahrs_int = Net('ahrs_int')
-    yield resistor(5.6e3)('R3', A=vcc3_3, B=ahrs_i2c.SCL)
-    yield resistor(5.6e3)('R4', A=vcc3_3, B=ahrs_i2c.SDA)
-    U3 = yield BNO055.BNO055('U3',
-        GND=gnd,
-        VDD=vcc3_3,
-        nBOOT_LOAD_PIN=vcc3_3, # XXX supposed to have 10k pullup?
-        PS1=gnd,
-        PS0=gnd,
-        PIN10=gnd,
-        #nRESET
-        INT=ahrs_int,
-        PIN15=gnd,
-        PIN16=gnd,
-        COM3=gnd, # I2C_ADDR_SEL
-        COM2=gnd,
-        COM1=ahrs_i2c.SCL,
-        COM0=ahrs_i2c.SDA,
-        GNDIO=gnd,
+    sensor_spi = harnesses.SPIBus.new('sensor_spi_')
+    
+    # IMU
+    imu_spi_nCS = Net('imu_spi_nCS') # XXX connect to uC
+    imu_INT = Net('imu_INT') # XXX connect to uC
+    yield capacitor(10e-9)('U3C3', A=vcc3_3, B=gnd) # near VDDIO
+    yield capacitor(0.1e-6)('U3C2', A=vcc3_3, B=gnd) # near VDD
+    REGOUT = Net('REGOUT')
+    yield capacitor(0.1e-6)('U3C1', A=REGOUT, B=gnd)
+    yield MPU_9250.MPU_9250('U3',
+        RESV_1=vcc3_3,
         VDDIO=vcc3_3,
+        AD0_SDO=sensor_spi.MISO,
+        REGOUT=REGOUT,
+        #FSYNC=imu_FSYNC,
+        INT=imu_INT,
+        VDD=vcc3_3,
+        GND=gnd,
+        RESV_20=gnd,
+        nCS=imu_spi_nCS,
+        SCL_SCLK=sensor_spi.SCLK,
+        SDA_SDI=sensor_spi.MOSI,
     )
-    yield capacitor(100e-9)('U3C1', A=U3.pin.VDD, B=gnd)
-    yield capacitor(100e-9)('U3C2', A=U3.pin.CAP, B=gnd)
-    yield capacitor(120e-9)('U3C3', A=U3.pin.VDDIO, B=gnd)
-    yield capacitor(6.8e-9)('U3C4', A=U3.pin.VDDIO, B=gnd)
-    yield resistor(10e3)('U3R1', A=U3.pin.VDDIO, B=U3.pin.nRESET)
+    
+    # EXTERNAL IMUs
+    external_spi_nCS = [Net('external_spi_nCS_' + str(i)) for i in xrange(3)] # XXX connect these to uC
+    for i in xrange(3):
+        yield SH.SM06B_SRSS_TB('VCC GND SCLK MOSI MISO nCS'.split(' '))('P9_'+str(i),
+            VCC=vcc3_3,
+            GND=gnd,
+            SCLK=sensor_spi.SCLK,
+            MOSI=sensor_spi.MOSI,
+            MISO=sensor_spi.MISO,
+            nCS=external_spi_nCS[i],
+            MECHANICAL=gnd,
+        )
     
     # ALTIMETER
-    baro_spi = harnesses.SPIBus.new('baro_spi_')
     baro_spi_nCS = Net('baro_spi_nCS')
     yield capacitor(100e-9)('U4C', A=vcc3_3, B=gnd)
     yield MS5611_01BA03.MS5611_01BA03('U4',
@@ -175,9 +185,9 @@ def main():
         PS=gnd, # use SPI
         GND=gnd,
         CSB=baro_spi_nCS,
-        SDO=baro_spi.MISO,
-        SDI=baro_spi.MOSI,
-        SCLK=baro_spi.SCLK,
+        SDO=sensor_spi.MISO,
+        SDI=sensor_spi.MOSI,
+        SCLK=sensor_spi.SCLK,
     )
     
     # SD CARD
@@ -224,13 +234,14 @@ def main():
     uc_NRST = Net('uc_NRST')
     yield resistor(10e3)('R5', A=vcc3_3, B=uc_NRST)
     uc_SWO = Net('uc_SWO')
-    yield make_header('VREF SWCLK GND SWDIO NRST SWO'.split(' '))('P5',
+    yield SH.SM06B_SRSS_TB('VREF SWCLK GND SWDIO NRST SWO'.split(' '))('P5',
         VREF=vcc3_3,
         SWCLK=uc_SWCLK,
         GND=gnd,
         SWDIO=uc_SWDIO,
         NRST=uc_NRST,
-        SWO=uc_SWO,
+        SWO=uc_SWO, # XXX plug into something for LED
+        MECHANICAL=gnd,
     )
     
     for i in xrange(4):
@@ -238,7 +249,7 @@ def main():
     yield capacitor(4.7e-6)('U5C4', A=vcc3_3, B=gnd) # connect to VDD_3 (pin 48)
     yield capacitor(10e-9)('U5C6', A=vcc3_3, B=gnd) # connect to VDDA (pin 9)
     yield capacitor(1e-6)('U5C7', A=vcc3_3, B=gnd) # connect to VDDA (pin 9)
-    yield STM32F103.STM32F103CBT7('U5',
+    yield STM32F103.STM32F103CBU6('U5',
         VSS=gnd, VSSA=gnd,
         VDD=vcc3_3, VDDA=vcc3_3, VBAT=vcc3_3,
         
@@ -247,28 +258,41 @@ def main():
         
         #PA4=vbat_divided, # ADC12_IN4
         
-        PA2=port_rxd, # USART2_TX
-        PA3=port_txd, # USART2_RX
-        PA0=port_rts, # USART2_CTS
-        PA1=port_cts, # USART2_RTS
+        #PA2=port_rxd, # USART2_TX # XXX move to 5V tolerant pins
+        #PA3=port_txd, # USART2_RX
+        #PA0=port_rts, # USART2_CTS
+        #PA1=port_cts, # USART2_RTS
+        PB10=port_rxd, # USART3_TX (are 5V tolerant!)
+        PB11=port_txd, # USART3_RX
+        PB13=port_rts, # USART3_CTS
+        PB14=port_cts, # USART3_RTS
+        
+        #PB6 # USART1_TX (5V tolerant)
+        #PB7 # USART1_RX
 
         
+        #PA4=sd_spi_nCS, # SPI1_NSS
+        #PA5=sd_spi.SCLK, # SPI1_SCK
+        #PA6=sd_spi.MISO, # SPI1_MISO
+        #PA7=sd_spi.MOSI, # SPI1_MOSI
         PA15=sd_spi_nCS, # SPI1_NSS
         PB3=sd_spi.SCLK, # SPI1_SCK
         PB4=sd_spi.MISO, # SPI1_MISO
         PB5=sd_spi.MOSI, # SPI1_MOSI
         
-        PB2=ahrs_int,
-        PB10=ahrs_i2c.SCL, # I2C2_SCL
-        PB11=ahrs_i2c.SDA, # I2C2_SDA
+        #PB2=ahrs_int,
+        #PB10=ahrs_i2c.SCL, # I2C2_SCL
+        #PB11=ahrs_i2c.SDA, # I2C2_SDA
         
         PB12=baro_spi_nCS, # SPI2_NSS
-        PB13=baro_spi.SCLK, # SPI2_SCK
-        PB14=baro_spi.MISO, # SPI2_MISO
-        PB15=baro_spi.MOSI, # SPI2_MOSI
+        PB13=sensor_spi.SCLK, # SPI2_SCK
+        PB14=sensor_spi.MISO, # SPI2_MISO
+        PB15=sensor_spi.MOSI, # SPI2_MOSI
         
-        PA9 =gps_rx, # USART1_TX
+        PA9 =gps_rx, # USART1_TX (5V tolerant)
         PA10=gps_tx, # USART1_RX
+        #PA11 # UART1_CTS
+        #PA12 # USART1_RTS
         
         PA8=usb_host_sense, # need to be 5V tolerant!
         PA11=usb.Dm, # USBDM
