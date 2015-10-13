b3983da5615f82a6d2d207e3fe3dc7adf702e124
Forrest Voight <forrest@forre.st>
2015-10-13 18:46:36 -0400

    prev

--- imu_pcb/main.py
+++ imu_pcb/main.py
@@ -77,5 +77,5 @@ def main():
 
 desc = main()
 kicad.generate(desc, 'kicad')
-bom.generate(desc, 'bom', quantity=3, add_spares=True)
+bom.generate(desc, 'bom', quantity=6, add_spares=True)
 #netlist_graph2.generate(desc, 'netlist.svg')
